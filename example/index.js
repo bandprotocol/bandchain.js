e7f87d357348baed9620b3950fea164dfe52dbd9
Sawit Trisirisatayawong <sawit@bandprotocol.com>
2020-10-02 10:49:30 +0700

    added comments to example and updated package details

--- example/index.js
+++ example/index.js
@@ -1,13 +1,22 @@
 const BandChain = require('../lib/BandChain')
+
+// Library for working with Band's Oracle Binary Encoding
+// https://docs.bandchain.org/developer/technical-specifications/obi.html
 const { Obi } = require('@bandprotocol/obi.js')
 
+// BandChain's Proof-of-Authority REST endpoint
 const endpoint = 'http://poa-api.bandchain.org'
 const bandchain = new BandChain(endpoint)
 
+// Aggregator oracle script
 const oracleScriptID = 8
 const mnemonic =
   'fox board balance genuine oblige neglect gadget awesome debris laundry gallery oppose never pupil cabbage advance cause fiction hamster knee debate obey analyst grunt'
 
+// This example submits a new data request transaction to BandChain
+// and returns the associated request's ID
+// This ID can then be used to retrieve the corresponding request result
+// as shown in getRequestResult, exampleGetRequestEVMProof, and exampleGetRequestNonEVMProof
 async function exampleSubmitRequestTx() {
   const oracleScript = await bandchain.getOracleScript(oracleScriptID)
   const validatorCounts = { minCount: 3, askCount: 4 }
@@ -20,12 +29,16 @@ async function exampleSubmitRequestTx() {
   return requestID
 }
 
+// This example submits a new data request to BandChain and
+// and returns the result
 async function exampleGetRequestResult() {
   const requestID = await exampleSubmitRequestTx()
   const requestResult = await bandchain.getRequestResult(requestID)
   return requestResult
 }
 
+// This example demonstrates how to decode the OBI-encoded data returned
+// by BandChain using the helper OBI.js library
 async function exampleDecodeRequestResult() {
   const oracleScript = await bandchain.getOracleScript(oracleScriptID)
   const schema = oracleScript.schema
@@ -38,18 +51,17 @@ async function exampleDecodeRequestResult() {
   return decodedResult
 }
 
+// This example demonstrateshow to retrieve proof associated with a request
+// on BandChain to use in smart contracts through Band's bridge contract
+// https://github.com/bandprotocol/bandchain/tree/master/bridges/evm
 async function exampleGetRequestEVMProof() {
   const requestID = await exampleSubmitRequestTx()
   const evmProof = await bandchain.getRequestEVMProof(requestID)
   return evmProof
 }
 
-async function exampleGetRequestNonEVMProof() {
-  const requestID = await exampleSubmitRequestTx()
-  const nonEVMProof = await bandchain.getRequestNonEVMProof(requestID)
-  return nonEVMProof
-}
-
+// This example demonstrates how to query BandChain for the latest request result
+// that matches specific request parameters.
 async function exampleGetLastMatchingRequestResult() {
   const oracleScript = await bandchain.getOracleScript(oracleScriptID)
   const validatorCounts = { minCount: 3, askCount: 4 }
@@ -61,6 +73,8 @@ async function exampleGetLastMatchingRequestResult() {
   return result
 }
 
+// This example demonstrates how to query price data from
+// Band's standard dataset
 async function exampleGetReferenceData() {
   const rate = await bandchain.getReferenceData([
     'BTC/USD',
