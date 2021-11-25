df5f0fca9f160f5c80445ecab4daee3090dedc6d
babybun <wpakasame@gmail.com>
2021-11-25 17:27:26 +0700

    add nodeJs example

--- example/node/data_source.ts
+++ example/node/data_source.ts
@@ -4,21 +4,20 @@ import path from 'path'
 
 const grpcEndpoint = 'https://laozi-testnet4.bandchain.org/grpc-web'
 const client = new Client(grpcEndpoint)
+const { PrivateKey } = Wallet
+const mnemonic = 'test'
+const privateKey = PrivateKey.fromMnemonic(mnemonic)
+const pubkey = privateKey.toPubkey()
+const sender = pubkey.toAddress().toAccBech32()
 
 async function exampleCreateDataSource() {
-  const { PrivateKey } = Wallet
-  const mnemonic = 'test'
-  const privateKey = PrivateKey.fromMnemonic(mnemonic)
-  const pubkey = privateKey.toPubkey()
-  const sender = pubkey.toAddress().toAccBech32()
-
-  const execPath = path.resolve(__dirname, '../mock/sample_ds.py')
+  const execPath = path.resolve(__dirname, '../mock/example_data_source.py')
   const file = fs.readFileSync(execPath, 'utf8')
   const executable = Buffer.from(file).toString('base64')
   console.log(executable.length)
 
-  const owner = 'band18e55d9xyrgyg3tk72zmg7s92uu8sd95jzgj73a'
-  const treasury = 'band18e55d9xyrgyg3tk72zmg7s92uu8sd95jzgj73a'
+  // const owner = 'band18e55d9xyrgyg3tk72zmg7s92uu8sd95jzgj73a'
+  // const treasury = 'band18e55d9xyrgyg3tk72zmg7s92uu8sd95jzgj73a'
 
   let coin = new Coin()
   coin.setDenom('uband')
@@ -33,10 +32,60 @@ async function exampleCreateDataSource() {
     'Test DS NodeJs',
     executable,
     [feeCoin],
-    treasury,
-    owner,
+    sender,
+    sender,
     sender,
     'Test DS NodeJs Description',
+  )
+
+  // Step 3.1: Construct a transaction
+  const fee = new Fee()
+  fee.setAmountList([feeCoin])
+  fee.setGasLimit(1000000)
+  const chainId = await client.getChainId()
+  const txn = new Transaction()
+  txn.withMessages(requestMessage)
+  await txn.withSender(client, sender)
+  txn.withChainId(chainId)
+  txn.withFee(fee)
+  txn.withMemo('')
+
+  // Step 3.2: Sign the transaction using the private key
+  const signDoc = txn.getSignDoc(pubkey)
+  const signature = privateKey.sign(signDoc)
+
+  const txRawBytes = txn.getTxData(signature, pubkey)
+
+  // Step 4: Broadcast the transaction
+  const sendTx = await client.sendTxBlockMode(txRawBytes)
+
+  return sendTx
+}
+
+async function exampleEditDataSource() {
+  const execPath = path.resolve(__dirname, '../mock/example_data_source.py')
+  const file = fs.readFileSync(execPath, 'utf8')
+  const executable = Buffer.from(file).toString('base64')
+  console.log(executable.length)
+
+  let coin = new Coin()
+  coin.setDenom('uband')
+  coin.setAmount('1000000')
+
+  let feeCoin = new Coin()
+  feeCoin.setDenom('uband')
+  feeCoin.setAmount('1000')
+
+  // Step 2.2: Create an oracle request message
+  const requestMessage = new Message.MsgEditDataSource(
+    184,
+    executable,
+    [feeCoin],
+    sender,
+    sender,
+    sender,
+    'Test Edit DS NodeJs',
+    'Test Edit DS NodeJs Description',
   )
 
   // Step 3.1: Construct a transaction
@@ -66,4 +115,6 @@ async function exampleCreateDataSource() {
 ;(async () => {
   console.log('Creating a new data source...')
   console.log(await exampleCreateDataSource())
+  console.log('Editing a new data source...')
+  console.log(await exampleEditDataSource())
 })()
