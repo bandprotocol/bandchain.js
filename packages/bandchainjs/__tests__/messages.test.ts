7dd828fcfda507232d44ea98341744114776886e
babybunn <wpakasame@gmail.com>
2024-11-06 15:43:12 +0700

    test: add message test cases

--- packages/bandchainjs/__tests__/messages.test.ts
+++ packages/bandchainjs/__tests__/messages.test.ts
@@ -1,15 +1,40 @@
-import { cosmos } from "../src";
+import { band, cosmos } from "../src";
 
-it("cosmos", () => {
-  const message = cosmos.bank.v1beta1.MessageComposer.fromPartial.send({
-    amount: [
-      {
-        amount: "1",
-        denom: "uatom"
-      }
-    ],
-    fromAddress: "myaddress",
-    toAddress: "youraddress"
+describe("Cosmos messages", () => {
+  it("send", () => {
+    const message = cosmos.bank.v1beta1.MessageComposer.fromPartial.send({
+      amount: [
+        {
+          amount: "1",
+          denom: "uatom",
+        },
+      ],
+      fromAddress: "myaddress",
+      toAddress: "youraddress",
+    });
+    expect(message).toMatchSnapshot();
   });
-  expect(message).toMatchSnapshot();
+});
+
+describe("Band messages", () => {
+  it("MsgRequestData", () => {
+    const { requestData } = band.oracle.v1.MessageComposer.fromPartial;
+    const message = requestData({
+      oracleScriptId: BigInt(1),
+      calldata: new Uint8Array([1, 2, 3]),
+      askCount: BigInt(1),
+      minCount: BigInt(1),
+      clientId: "client",
+      feeLimit: [
+        {
+          denom: "uband",
+          amount: "1",
+        },
+      ],
+      prepareGas: BigInt(1),
+      executeGas: BigInt(1),
+      sender: "sender",
+    });
+    expect(message).toMatchSnapshot();
+  });
 });
